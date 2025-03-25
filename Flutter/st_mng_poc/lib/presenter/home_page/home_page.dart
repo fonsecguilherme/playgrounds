6c64c0d997c661f80e405ce5081189e228ed6bd0
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-03-24 23:09:59 -0300

    removal: appbar and material app widget

--- Flutter/st_mng_poc/lib/presenter/home_page/home_page.dart
+++ Flutter/st_mng_poc/lib/presenter/home_page/home_page.dart
@@ -17,82 +17,74 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     _vm = HomePageVm();
-    // _vm.onInit();
   }
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Video Demo',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('State Management'),
-            backgroundColor: Colors.red,
-          ),
-          backgroundColor: Colors.white,
-          body: ValueListenableBuilder(
-            valueListenable: _vm.state,
-            builder: (context, state, _) {
-              if (state.status == UserStatus.error) {
-                return Center(child: Text('deu erroooooooooooooooooooooooo'));
-              }
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Colors.white,
+        body: ValueListenableBuilder(
+          valueListenable: _vm.state,
+          builder: (context, state, _) {
+            if (state.status == UserStatus.error) {
+              return Center(child: Text('deu erroooooooooooooooooooooooo'));
+            }
 
-              if (state.loading) {
-                return Center(child: const Text('carregando...'));
-              }
+            if (state.loading) {
+              return Center(child: const Text('carregando...'));
+            }
 
-              if (state.user.isEmpty) {
-                return Center(child: const Text('Estado inicial...'));
-              }
+            if (state.user.isEmpty) {
+              return Center(child: const Text('Estado inicial...'));
+            }
 
-              if (state.user.isNotEmpty) {
-                return Center(
-                  child: Text(
-                    'Estado de sucesso... ${state.user.first.name} '
-                    'e a idade é: ${state.user.first.age}',
-                  ),
-                );
-              }
+            if (state.user.isNotEmpty) {
+              return Center(
+                child: Text(
+                  'Estado de sucesso... ${state.user.first.name} '
+                  'e a idade é: ${state.user.first.age}',
+                ),
+              );
+            }
 
-              return SizedBox.shrink();
+            return SizedBox.shrink();
 
-              // switch (state.runtimeType) {
-              // case const (HomeScreenLoadingState):
-              //   return const Center(child: CircularProgressIndicator());
-              // case const (HomeScreenSuccessfulState):
-              //   final successfulState = state as HomeScreenSuccessfulState;
-              //   return Center(child: Text(successfulState.userName));
-              // case const (HomeScreenErrorState):
-              //   final errorState = state as HomeScreenErrorState;
-              //   return Center(child: Text(errorState.errorMessage));
-              // default:
-              //   return Container();
-              // }
-            },
-          ),
-          floatingActionButton: Row(
-            spacing: 16.0,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FloatingActionButton(
-                onPressed: () {
-                  _vm.resetAppState();
-                },
-                child: const Text('initial'),
-              ),
-              FloatingActionButton(
-                onPressed: () {
-                  _vm.errorUserData();
-                },
-                child: const Text('erro'),
-              ),
-              FloatingActionButton(
-                onPressed: () {
-                  _vm.onRefreshUserData();
-                },
-                child: const Text('success'),
-              ),
-            ],
-          ),
+            // switch (state.runtimeType) {
+            // case const (HomeScreenLoadingState):
+            //   return const Center(child: CircularProgressIndicator());
+            // case const (HomeScreenSuccessfulState):
+            //   final successfulState = state as HomeScreenSuccessfulState;
+            //   return Center(child: Text(successfulState.userName));
+            // case const (HomeScreenErrorState):
+            //   final errorState = state as HomeScreenErrorState;
+            //   return Center(child: Text(errorState.errorMessage));
+            // default:
+            //   return Container();
+            // }
+          },
+        ),
+        floatingActionButton: Row(
+          spacing: 16.0,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                _vm.resetAppState();
+              },
+              child: const Text('initial'),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                _vm.errorUserData();
+              },
+              child: const Text('erro'),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                _vm.onRefreshUserData();
+              },
+              child: const Text('success'),
+            ),
+          ],
         ),
       );
 }
