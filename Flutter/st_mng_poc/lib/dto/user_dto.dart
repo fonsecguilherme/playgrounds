c601b9bc3aeea34b7a0a1b0a48427d34f1bf9608
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-03-26 23:44:48 -0300

    feat: auxiliary mathods to acess and retrive data from DB

--- Flutter/st_mng_poc/lib/dto/user_dto.dart
+++ Flutter/st_mng_poc/lib/dto/user_dto.dart
@@ -1,6 +1,21 @@
+import 'package:st_mng_poc/database.dart';
+
 class UserDto {
   final String name;
   final int? age;
 
   UserDto({required this.name, this.age = 0});
+
+  UsersCompanion toCompanion() {
+    return UsersCompanion.insert(name: name, age: age ?? 0);
+  }
+
+  factory UserDto.fromDatabase(User user) {
+    return UserDto(name: user.name, age: user.age);
+  }
+
+  @override
+  String toString() {
+    return 'UserDTO(nome: $name, age: $age)';
+  }
 }
