0cce0444b16211d6bfae610b8f86b9d4670bc2ba
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-03-26 23:45:46 -0300

    feat: drif db implementation

---
+++ Flutter/st_mng_poc/lib/data/drift_database.dart
@@ -0,0 +1,30 @@
+import 'package:st_mng_poc/domain/i_database.dart';
+
+import '../database.dart';
+import '../dto/user_dto.dart';
+
+class DriftDatabase implements IDatabase {
+  final AppDatabase _database;
+
+  DriftDatabase({
+    required AppDatabase database,
+  }) : _database = database;
+
+  @override
+  Future<T> insert<T>(T data) async {
+    if (data is UsersCompanion) {
+      await _database.into(_database.users).insert(data);
+    }
+
+    throw UnimplementedError('O tipo passado para o banco não é suportado');
+  }
+
+  @override
+  Future<List<T>> select<T>() async {
+    if (T == UserDto) {
+      final users = await _database.select(_database.users).get();
+      return users.map((user) => UserDto.fromDatabase(user) as T).toList();
+    }
+    throw Exception('Unsupported type for select');
+  }
+}
