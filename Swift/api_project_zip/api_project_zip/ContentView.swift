81b6c9c3b80ea41b5021e66cff97601783271e0e
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-04-05 15:03:01 -0300

    Arquivos do projeto

---
+++ Swift/api_project_zip/api_project_zip/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift 
+//  api_project_zip
+//
+//  Created by Guilherme Fonseca on 05/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeViewPage()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift/api_project_zip/api_project_zip/api_project_zipApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_project_zipApp.swift
+//  api_project_zip
+//
+//  Created by Guilherme Fonseca on 05/04/25.
+//
+
+import SwiftUI
+
+@main
+struct api_project_zipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
