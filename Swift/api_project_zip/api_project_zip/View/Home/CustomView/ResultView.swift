ade17cb9fa318c01411b5e2f558ddf28843a05bd
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-04-20 18:31:14 -0300

    feat: result view creation

---
+++ Swift/api_project_zip/api_project_zip/View/Home/CustomView/ResultView.swift
@@ -0,0 +1,19 @@
+//
+//  ResultView.swift
+//  api_project_zip
+//
+//  Created by Guilherme Fonseca on 20/04/25.
+//
+
+import SwiftUI
+
+struct ResultView: View {    
+    let vm: HomePageVM
+    
+    var body: some View {
+        VStack {
+            CustomCardView( logradouro: vm.safeAddress.logradouro, cep: vm.safeAddress.cep, cidade: String(vm.safeAddress.cep), estado: vm.safeAddress.estado)
+                .padding()
+        }
+    }
+}
