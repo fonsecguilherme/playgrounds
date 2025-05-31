10981069779bfcf5c6d2fd5085d0ad415d80c2b2
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-05-31 16:35:05 -0300

    WIP: loading appbar

---
+++ Flutter/rive_poc/lib/rive_asset.dart
@@ -0,0 +1,59 @@
+import 'package:rive/rive.dart';
+
+class RiveAsset {
+  final String artboard, stateMachineName, title, src;
+  late SMIBool? input;
+
+  RiveAsset(
+    this.src, {
+    required this.artboard,
+    required this.stateMachineName,
+    required this.title,
+    this.input,
+  });
+
+  set setInput(SMIBool status) {
+    input = status;
+  }
+}
+
+List<RiveAsset> bottomNavs = [
+  RiveAsset(
+    'assets/animated_icons.riv',
+    artboard: 'HOME',
+    stateMachineName: 'HOME_interactivity',
+    title: 'Home',
+  ),
+  RiveAsset(
+    'assets/animated_icons.riv',
+    artboard: 'SEARCH',
+    stateMachineName: 'SEARCH_Interactivity',
+    title: 'Search',
+  ),
+  RiveAsset(
+    'assets/animated_icons.riv',
+    artboard: 'LIKE/STAR',
+    stateMachineName: 'STAR_Interactivity',
+    title: 'Like',
+  )
+];
+List<RiveAsset> whiteBottomNavItems = [
+  RiveAsset(
+    'assets/animated_icons_white.riv',
+    artboard: 'HOME',
+    stateMachineName: 'HOME_interactivity',
+    title: 'Home',
+  ),
+  RiveAsset(
+    'assets/animated_icons_white.riv',
+    artboard: 'SEARCH',
+    stateMachineName: 'SEARCH_Interactivity',
+    title: 'Search',
+  ),
+  RiveAsset(
+    'assets/animated_icons_white.riv',
+    artboard: 'LIKE/STAR',
+    stateMachineName: 'STAR_Interactivity',
+    title: 'Like',
+  ),
+];
--- Flutter/rive_poc/lib/src/settings/settings_view.dart
+++ Flutter/rive_poc/lib/src/settings/settings_view.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+import 'package:rive_poc/rive_asset.dart';
 
+import '../../utils/rive_utils.dart';
 import 'settings_controller.dart';
 
 /// Displays the various settings that can be customized by the user.
 ///
 /// When a user changes a setting, the SettingsController is updated and
 /// Widgets that listen to the SettingsController are rebuilt.
-class SettingsView extends StatelessWidget {
+class SettingsView extends StatefulWidget {
   const SettingsView({super.key, required this.controller});
 
   static const routeName = '/settings';
 
   final SettingsController controller;
+
+  @override
+  State<SettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  RiveAsset selectedBottomNav = bottomNavs.first;
+
+  late RiveAnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = SimpleAnimation('indle');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,25 +43,107 @@ class SettingsView extends StatelessWidget {
         //
         // When a user selects a theme from the dropdown list, the
         // SettingsController is updated, which rebuilds the MaterialApp.
-        child: DropdownButton<ThemeMode>(
-          // Read the selected themeMode from the controller
-          value: controller.themeMode,
-          // Call the updateThemeMode method any time the user selects a theme.
-          onChanged: controller.updateThemeMode,
-          items: const [
-            DropdownMenuItem(
-              value: ThemeMode.system,
-              child: Text('System Theme'),
+        child: Column(
+          children: [
+            DropdownButton<ThemeMode>(
+              // Read the selected themeMode from the controller
+              value: widget.controller.themeMode,
+              // Call the updateThemeMode method any time the user selects a theme.
+              onChanged: widget.controller.updateThemeMode,
+              items: const [
+                DropdownMenuItem(
+                  value: ThemeMode.system,
+                  child: Text('System Theme'),
+                ),
+                DropdownMenuItem(
+                  value: ThemeMode.light,
+                  child: Text('Light Theme'),
+                ),
+                DropdownMenuItem(
+                  value: ThemeMode.dark,
+                  child: Text('Dark Theme'),
+                )
+              ],
             ),
-            DropdownMenuItem(
-              value: ThemeMode.light,
-              child: Text('Light Theme'),
+            SizedBox(
+              height: 200,
+              width: double.infinity,
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _controller.isActive = !_controller.isActive;
+                  });
+                },
+                child: RiveAnimation.asset(
+                  widget.controller.themeMode == ThemeMode.dark
+                      ? 'assets/animated_icons_white.riv'
+                      : 'assets/animated_icons.riv',
+                  controllers: [_controller],
+                  fit: BoxFit.contain,
+                ),
+              ),
             ),
-            DropdownMenuItem(
-              value: ThemeMode.dark,
-              child: Text('Dark Theme'),
-            )
           ],
+        ),
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 24),
+          decoration: BoxDecoration(
+            color: widget.controller.themeMode == ThemeMode.dark
+                ? Colors.white
+                : Colors.black,
+            borderRadius: const BorderRadius.all(Radius.circular(24)),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ...List.generate(
+                bottomNavs.length,
+                (index) => GestureDetector(
+                  onTap: () {
+                    bottomNavs[index].input!.change(true);
+                    if (bottomNavs[index] != selectedBottomNav) {
+                      setState(() {
+                        selectedBottomNav = bottomNavs[index];
+                      });
+                    }
+                    Future.delayed(const Duration(seconds: 1), () {
+                      bottomNavs[index].input!.change(false);
+                    });
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // TODO: Animated Bar
+                      SizedBox(
+                        height: 36,
+                        width: 36,
+                        child: Opacity(
+                          opacity:
+                              bottomNavs[index] == selectedBottomNav ? 1 : 0.5,
+                          child: RiveAnimation.asset(
+                            bottomNavs[index].src,
+                            artboard: bottomNavs[index].artboard,
+                            onInit: (artboard) {
+                              StateMachineController controller =
+                                  RiveUtils.getRiveController(artboard,
+                                      stateMachineName:
+                                          bottomNavs[index].stateMachineName);
+
+                              bottomNavs[index].input =
+                                  controller.findSMI("active") as SMIBool;
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
