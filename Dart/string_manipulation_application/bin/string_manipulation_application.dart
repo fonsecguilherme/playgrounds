d2fb4313d211ed905e61c4b1ecdefc3c0987b868
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-11-07 23:09:16 -0300

    feat: updated file

--- Dart/string_manipulation_application/bin/string_manipulation_application.dart
+++ Dart/string_manipulation_application/bin/string_manipulation_application.dart
@@ -1,5 +1,1 @@
-import 'package:string_manipulation_application/string_manipulation_application.dart' as string_manipulation_application;
-
-void main(List<String> arguments) {
-  print('Hello world: ${string_manipulation_application.calculate()}!');
-}
+void main(List<String> arguments) {}
